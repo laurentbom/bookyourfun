29fe059f8404567bae6e9079e26563b6149311f8
LaurentB <laurent.bomal@gmail.com>
2023-05-13 18:21:12 +0200

    footerinprogress

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+//////////////////////////////////////////////////////
+// BookYourFun - Student work with ticketmaster API
+// Laurent Bomal 2023
+// Vue.js
+// 
+// - Responsive Treshold
+// - Get segments & genres for main nav
+// - Mobile menu
+// - Get segments & genres for footer nav
+//////////////////////////////////////////////////////
+
 const apiUrl = "https://app.ticketmaster.com/discovery/v2"
 const apiKey = "7lJGVPGW3rX8GgR7sYZnrgDpbWTFFfQA";
 
@@ -6,6 +17,7 @@ const idSegmentSport = "KZFzniwnSyZfZ7v7nE";
 const idSegmentArt = "KZFzniwnSyZfZ7v7na";
 const idSegmentAttractions = "KZFzniwnSyZfZ7v7n1";
 const idSegmentFilms = "KZFzniwnSyZfZ7v7nn";
+
 
 function monApi2(idSegments){
   fetch(`${apiUrl}/classifications?apikey=${apiKey}?countryCode=be`)
@@ -19,20 +31,50 @@ function monApi2(idSegments){
 }
 
 
-monApi2();
+// monApi2();
 
+////////////////////App Vue////////////////////////
 const {createApp} = Vue;
 
 createApp({
   data(){
     return{
+      navLinks : [
+        {
+          name : "Music",
+          id : "KZFzniwnSyZfZ7v7nJ",
+        },
+        {
+          name : "Arts and Theater",
+          id : "KZFzniwnSyZfZ7v7na",
+        },
+        {
+          name : "Films",
+          id : "KZFzniwnSyZfZ7v7nn",
+        },
+        {
+          name : "Sports",
+          id : "KZFzniwnSyZfZ7v7nE",
+        },
+        {
+          name : "Miscellaneous",
+          id : "KZFzniwnSyZfZ7v7n1",
+        }
+      ],
       genres: [],
+      genresFooter: [],
       segment: [],
       mobileNavClass : "",
       burgerNavClass : "",
       isMobile: false,
+      navIsHover: {},
+      showDropdown: false,
+      activeLinkId: "",
+      genresFooterDesktop: [],
     }
   },
+
+  ///////////////////Responsive threshold////////////////////////
   created() {
     // Vérifie la résolution de l'écran et définit la propriété isMobile
     const mediaQuery = window.matchMedia('(max-width: 1300px)')
@@ -44,6 +86,8 @@ createApp({
     })
   },
   methods : {
+
+    ////////////////Get segments & genres for main nav////////////////////
     getGenres(idSegments){
       fetch(`${apiUrl}/classifications/segments/${idSegments}?apikey=${apiKey}`)
       .then(response => response.json())
@@ -64,6 +108,8 @@ createApp({
       .catch(error => {
         console.error("Erreur lors de la récupération des données :", error); 
       });
+
+      /////////////////Mobile menu/////////////////
       if(this.isMobile){
         this.mobileNavClass = "";
         this.burgerNavClass = "";
@@ -83,9 +129,55 @@ createApp({
     },
     listAppLaunch(){
       this.genres = [];
-    }
+    },
+    ////////////////Get segments & genres for footer nav////////////////////
+    getGenresFooterMobile(idSegments) {
+      // Si le lien cliqué est le même que la navLink active, on ferme tous les sous-menus
+      if (this.activeLinkId === idSegments) {
+        this.showDropdown = false;
+        this.activeLinkId = "";
+      }
+      // Sinon on met à jour l'activeLink
+      else {
+        this.showDropdown = true;
+        this.activeLinkId = idSegments;
+      }
+
+      fetch(`${apiUrl}/classifications/segments/${idSegments}?apikey=${apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.name);
+        this.genresFooter = [];
+        for(i=0; i < 6; i++){
+          let genreFooter = data._embedded.genres[i];
+          if(genreFooter && genreFooter.name && genreFooter.id && genreFooter.name != "Undefined"){
+            this.genresFooter.push({name: genreFooter.name, id: genreFooter.id});
+          }
+        };
+      })
+      .catch(error => {
+        console.error("Erreur lors de la récupération des données :", error); 
+      });
+    },
+    ////////////////Get segments & genres for footer nav////////////////////
+    getGenresFooterDesktop(idSegments) {
+      fetch(`${apiUrl}/classifications/segments/${idSegments}?apikey=${apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        this.genresFooterDesktop = [];
+        for(i=0; i < 20; i++){
+          let genreFooterDesktop = data._embedded.genres[i];
+          if(genreFooterDesktop && genreFooterDesktop.name && genreFooterDesktop.id && genreFooterDesktop.name != "Undefined"){
+            this.genresFooterDesktop.push({name: genreFooterDesktop.name, id: genreFooterDesktop.id});
+          }
+        };
+      })
+      .catch(error => {
+        console.error("Erreur lors de la récupération des données :", error); 
+      });
+    },
   },
   mounted(){
-    // this.getGenres();
+    this.getGenresFooterDesktop(idSegmentSport);
   }
 }).mount("#byfApp")
